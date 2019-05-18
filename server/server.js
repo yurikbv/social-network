5335f2904f10837df58d9aaeaaf976f89fc98039
yurikbv <yurikbvv@gmail.com>
2019-05-18 13:52:44 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -47,6 +47,8 @@ app.use(function (err, req, res, next) {
   }
 });
 
+app.use(express.static('client/build'));
+
 if(process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.get('/*', (req, res) => {
@@ -54,7 +56,7 @@ if(process.env.NODE_ENV === 'production') {
   })
 }
 
-app.use(express.static('client/build'));
+
 
 const port = process.env.PORT || 3002;
 
